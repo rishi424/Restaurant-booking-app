87def49428156e43638a692a580f0ba87249ee9e
Yaswant <24mrri@gmail.com>
2025-01-06 00:30:18 +0530

    remove cors

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const db = require('./db/dataBase');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-const corsOptions = {
-    origin: process.env.FRONTEND, // Replace with your frontend URL
-    methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
-    credentials: true, // Allow cookies and credentials
-  };
+
 // Middleware
 app.use(express.json());
-app.use(cors(corsOptions)); // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes
 morgan.token('req-body', (req) => JSON.stringify(req.body)); // Log request payload
 morgan.token('res-body', (req, res) => JSON.stringify(res.body)); // Log response body
 morgan.token('error-message', (req, res) => res.locals.errorMessage || ''); // Log error message
