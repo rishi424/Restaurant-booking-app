47c5a185c9c736f2b32f24f2b224bf4e11024178
Yaswant <24mrri@gmail.com>
2025-01-06 00:01:40 +0530

    db changes

--- db/dataBase.js
+++ db/dataBase.js
@@ -2,7 +2,12 @@ const mongoose= require("mongoose")
 
 const db=()=>{
     mongoose
-    .connect(process.env.DB_URI)
+    .connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 60000, // Increase connection timeout to 60 seconds
+        socketTimeoutMS: 60000,  // Increase socket timeout to 60 seconds
+      })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB', err));
 }
